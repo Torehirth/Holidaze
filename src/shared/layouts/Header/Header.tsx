35596b4aec3214e531ac0c0d5a1335d98be29f30
Tore Hirth <torehirth@gmail.com>
2026-07-16 12:02:55 +0200

    feat: add bottom border on active nav links

--- src/shared/layouts/Header/Header.tsx
+++ src/shared/layouts/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 import { Menu, User, X } from "lucide-react";
 import logo from "../../assets/logo/logo_small.svg";
 
@@ -10,39 +10,63 @@ export const Header = () => {
     setIsMenuOpen(false);
   };
 
+  const initialClass = `px-2 hover:opacity-70 active:scale-90`;
+  const activeClass = `border-primary border-b ${initialClass}`;
+  const initialClassMobile = `px-2 hover:opacity-70 active:scale-90`;
+  const activeClassMobile = `border-background border-b border- ${initialClass}`;
+
   return (
     <header className="border-border-light relative h-20 border-b">
       <div className="flex h-full items-center justify-between px-8">
-        <Link to="/" aria-label="Go to Holidaze homepage">
+        <NavLink
+          to="/"
+          className="hover:opacity-80 active:scale-95"
+          aria-label="Go to Holidaze homepage">
           <img src={logo} alt="" />
-        </Link>
+        </NavLink>
 
         {/* Desktop navigation */}
         <nav
           aria-label="Primary navigation"
           className="hidden items-center gap-12 md:flex">
-          <Link to="/">Home</Link>
-          <Link to="/venues">Explore</Link>
-          <Link to="/profile">Dashboard</Link>
-          <Link to="/host">Host</Link>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? activeClass : initialClass)}>
+            Home
+          </NavLink>
+          <NavLink
+            to="/venues"
+            className={({ isActive }) => (isActive ? activeClass : initialClass)}>
+            Explore
+          </NavLink>
+          <NavLink
+            to="/profile"
+            className={({ isActive }) => (isActive ? activeClass : initialClass)}>
+            Dashboard
+          </NavLink>
+          <NavLink
+            to="/host"
+            className={({ isActive }) => (isActive ? activeClass : initialClass)}>
+            Host
+          </NavLink>
         </nav>
 
         <div className="flex items-center gap-4">
-          <Link
+          <NavLink
             to="/profile"
             aria-label="Go to profile"
-            className="inline-flex items-center justify-center">
+            className="inline-flex items-center justify-center hover:opacity-70 active:scale-90">
             <User aria-hidden="true" className="text-foreground" />
-          </Link>
+          </NavLink>
 
           <button
             type="button"
             aria-label={isMenuOpen ? "Close navigation menu" : "Open navigation menu"}
             aria-expanded={isMenuOpen}
             aria-controls="mobile-navigation"
-            className="inline-flex items-center justify-center md:hidden"
+            className="inline-flex items-center justify-center hover:opacity-70 active:scale-90 md:hidden"
             onClick={() => {
-              setIsMenuOpen((previousState) => !previousState);
+              setIsMenuOpen((prevState) => !prevState);
             }}>
             {isMenuOpen ? <X aria-hidden="true" /> : <Menu aria-hidden="true" />}
           </button>
@@ -55,21 +79,46 @@ export const Header = () => {
         aria-label="Mobile navigation"
         aria-hidden={!isMenuOpen}
         className={`bg-foreground text-background fixed top-20 right-0 bottom-0 z-500 flex w-full max-w-sm flex-col items-center gap-4 px-8 pt-16 pb-8 text-center transition-transform duration-300 ease-in-out md:hidden ${isMenuOpen ? "translate-x-0" : "translate-x-[200%]"} `}>
-        <Link to="/" onClick={closeMenu} className="px-4 py-2">
+        <NavLink
+          to="/"
+          onClick={closeMenu}
+          className={({ isActive }) =>
+            isActive ? activeClassMobile : initialClassMobile
+          }>
           Home
-        </Link>
-        <Link to="/venues" onClick={closeMenu} className="px-4 py-2">
+        </NavLink>
+        <NavLink
+          to="/venues"
+          onClick={closeMenu}
+          className={({ isActive }) =>
+            isActive ? activeClassMobile : initialClassMobile
+          }>
           Venues
-        </Link>
-        <Link to="/dashboard" onClick={closeMenu} className="px-4 py-2">
+        </NavLink>
+        <NavLink
+          to="/dashboard"
+          onClick={closeMenu}
+          className={({ isActive }) =>
+            isActive ? activeClassMobile : initialClassMobile
+          }>
           Dashboard
-        </Link>
-        <Link to="/host" onClick={closeMenu} className="px-4 py-2">
+        </NavLink>
+        <NavLink
+          to="/host"
+          onClick={closeMenu}
+          className={({ isActive }) =>
+            isActive ? activeClassMobile : initialClassMobile
+          }>
           Host
-        </Link>
-        <Link to="/login" onClick={closeMenu} className="px-4 py-2">
+        </NavLink>
+        <NavLink
+          to="/login"
+          onClick={closeMenu}
+          className={({ isActive }) =>
+            isActive ? activeClassMobile : initialClassMobile
+          }>
           Login
-        </Link>
+        </NavLink>
       </nav>
 
       {/* Backdrop to close menu on click outside */}

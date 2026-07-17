32099e29ecf4dc632561641fb2e2b2dcab512227
Tore Hirth <torehirth@gmail.com>
2026-07-17 19:09:26 +0200

    refactor: delete unnecessary links

--- src/features/home/pages/HomePage.tsx
+++ src/features/home/pages/HomePage.tsx
@@ -1,26 +1,9 @@
-// import { Link } from "react-router";
-
 export const HomePage = () => {
   return (
     <>
-      {/* <div className="px-2 py-40">
-        <h1 className="p-2 pb-4">Home Page</h1>
-        <Link to="/venues" className="px-2">
-          Venues
-        </Link>
-        <Link to="/login" className="px-2">
-          Login
-        </Link>
-        <Link to="/dashboard" className="px-2">
-          Dashboard
-        </Link>
-        <Link to="/host" className="px-2">
-          Host
-        </Link>
-        <Link to="/register" className="px-2">
-          Register
-        </Link>
-      </div> */}
+      <div className="flex flex-col gap-8 px-2 py-8">
+        <h1 className="p-2 pb-4 text-2xl">Home Page</h1>
+      </div>
     </>
   );
 };
